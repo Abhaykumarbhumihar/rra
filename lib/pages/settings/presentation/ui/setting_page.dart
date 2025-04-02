27f678a2d7e0f1831a0b635454fb44788d854edc
Abhaykumarbhumihar <abhayrai637@gmail.com>
2025-04-02 19:27:55 +0530

    logout implement

--- lib/pages/settings/presentation/ui/setting_page.dart
+++ lib/pages/settings/presentation/ui/setting_page.dart
@@ -1,25 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:rra/common/component/common_page_format.dart';
 import 'package:rra/common/component/component_export.dart';
 
+import '../../../../common/local/SharedPrefs.dart';
 import '../../../../common/routes/exports.dart';
+import '../../../../common/routes/routes.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CommonPageFormat(title: "Setting",  onBackPress: (){},
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 50,),
+    return CommonPageFormat(
+        title: "Setting",
+        onBackPress: () {},
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 38.0, right: 38),
+              child: CustomButton(
+                  text: "Logout",
+                  onPressed: () {
+                    showLogoutConfirmationDialog(
+                        context: context,
+                        onCancel: () {
+                        //  Navigator.pop(context);
+                        },
+                        onLogout: () async {
+                          await SharedPrefs.remove("user_model");
+                          await SharedPrefs.remove("selected_academyid");
+                          await SharedPrefs.remove("stripe_auth_key");
+                          await SharedPrefs.remove("stripe_publish_key");
+                          await SharedPrefs.clear();
 
-          Padding(
-            padding: const EdgeInsets.only(left: 38.0,right: 38),
-            child: CustomButton(text: "Logout", onPressed: (){
+                          BlocProvider.of<EditprofileBloc>(context)
+                              .loadUserData();
+                          BlocProvider.of<AppBloc>(context).loadUserData();
 
-            }),
-          )
-        ],
-      ));
+                          Navigator.pushNamedAndRemoveUntil(
+                            context,
+                            AppRoutes.ACADEMICLISTPAGE,
+                            (Route<dynamic> route) => false,
+                          );
+                        });
+                  }),
+            )
+          ],
+        ));
+  }
+
+  void showLogoutConfirmationDialog({
+    required BuildContext context,
+    required VoidCallback onCancel,
+    required VoidCallback onLogout,
+  }) {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text('Logout'),
+          content: Text('Are you sure you want to log out?'),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+                onCancel(); // Call the provided onCancel callback
+              },
+            ),
+            CupertinoDialogAction(
+              child: Text('Logout'),
+              isDestructiveAction: true,
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+                onLogout(); // Call the provided onLogout callback
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
