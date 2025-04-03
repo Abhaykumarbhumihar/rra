8687350e10b264b04c757c2542c3bb16b9d7e2a8
Abhaykumarbhumihar <abhayrai637@gmail.com>
2025-04-03 19:22:08 +0530

    routes imort set

--- lib/common/routes/exports.dart
+++ lib/common/routes/exports.dart
@@ -20,3 +20,63 @@ export 'package:rra/pages/auth/resetpassword/presentation/bloc/resetpassword_blo
 export 'package:rra/pages/auth/edit_profile/presentation/bloc/editprofile_bloc.dart';
 export 'package:rra/pages/parents/application/presentatioin/bloc/app_bloc.dart';
 export 'package:rra/pages/parents/document/add_view_document/presentation/bloc/add_document_bloc.dart';
+
+export 'package:rra/common/routes/routes.dart';
+
+// Facility Pages
+export 'package:rra/pages/parents/facility/facility_add_to_cart/presentaion/ui/facility_add_to_cart_page.dart';
+export 'package:rra/pages/parents/facility/facility_detail/presentation/ui/facility_detail_page.dart';
+export 'package:rra/pages/parents/facility/facilitylist/presentation/ui/facility_list.dart';
+
+// Location
+export 'package:rra/pages/parents/location/presentation/bloc/location_bloc.dart';
+export 'package:rra/pages/parents/location/presentation/ui/location_list_page.dart';
+
+// Academic
+export '../../pages/academic/presentation/bloc/academic_bloc.dart';
+export '../../pages/academic/presentation/ui/academic_list_page.dart';
+
+// Authentication
+export '../../pages/auth/forgotpassword/presentation/bloc/forget_password_bloc.dart';
+export '../../pages/auth/forgotpassword/presentation/ui/forgot_password.dart';
+
+// Coach Attendance
+export '../../pages/coach/coach_attendance/player_attendance_list/presentation/ui/coach_player_attendance_list.dart';
+export '../../pages/coach/coach_attendance/single_player_attendance_page/presentation/ui/coach_single_player_detail_page.dart';
+
+// Coach Player Report
+export '../../pages/coach/coach_player_report/coach_player_report_detail/presentation/ui/coach_player_report_detail_page.dart';
+export '../../pages/coach/coach_player_report/coach_player_report_list/presentation/ui/coach_player_reort_list_page.dart';
+export '../../pages/coach/coach_player_report/coach_player_reprt_graph/presentation/ui/coach_player_report_graph_page.dart';
+
+// Coach Collaterals
+export '../../pages/coach/collaterals/collateral_details/presentation/ui/coach_my_collateral_detail.dart';
+export '../../pages/coach/collaterals/collaterals_list/presentation/ui/my_collateral_list.dart';
+
+// Coach Management
+export '../../pages/coach/manage_team/presentation/ui/manage_team_list.dart';
+export '../../pages/coach/view_session/presentation/ui/view_sessioin.dart';
+
+// Parents - Holiday Camp
+export '../../pages/parents/holiday_camp/holiday_detail/presentation/ui/holiday_detail_page.dart';
+export '../../pages/parents/holiday_camp/holiday_list/presentation/ui/holiday_campp.dart';
+
+// Parents - Orders
+export '../../pages/parents/parent_order/parent_order_detail/presentation/ui/parent_order_detail.dart';
+export '../../pages/parents/parent_order/parent_order_list/presentation/ui/parent_order_list_page.dart';
+
+// Parents - Player Score & Attendance
+export '../../pages/parents/parent_score_view/parent_player_score_list/presentation/ui/parent_player_score_list_page.dart';
+export '../../pages/parents/parent_view_attendance/presentation/ui/parent_attendance_list_age.dart';
+
+// Parents - Session Management
+export '../../pages/parents/session/add_detail/presentation/bloc/add_view_player_bloc.dart';
+export '../../pages/parents/session/add_detail/presentation/ui/add_detail.dart';
+export '../../pages/parents/session/calendar/presentation/bloc/session_calendar_bloc.dart';
+export '../../pages/parents/session/calendar/presentation/ui/calendart_page.dart';
+export '../../pages/parents/session/coaching_detail/presentation/bloc/coaching_detail_bloc.dart';
+export '../../pages/parents/session/coaching_detail/presentation/ui/coaching_detail.dart';
+export '../../pages/parents/session/coachprograms/presentation/bloc/coach_programs_bloc.dart';
+export '../../pages/parents/session/coachprograms/presentation/ui/coach_programs.dart';
+export '../../pages/parents/session/order_summary/presentation/bloc/order_summary_bloc.dart';
+export '../../pages/parents/session/order_summary/presentation/order_summary.dart';
