463725793a4efdadfaa5e61f89ebd732a8e31eb3
Abhaykumarbhumihar <abhayrai637@gmail.com>
2025-04-03 19:32:55 +0530

    updated code

---
+++ lib/common/service_locator/service_export.dart
@@ -0,0 +1,65 @@
+// 📌 Academic
+export '../../pages/academic/data/repoimpl/academic_repo_impl.dart';
+export '../../pages/academic/domain/repositery/academic_repoitery.dart';
+export '../../pages/academic/domain/usecase/academic_usecase.dart';
+
+// 📌 Authentication
+export '../../pages/auth/createaccount/data/repoImpl/create_account_impl.dart';
+export '../../pages/auth/createaccount/domain/repositery/create_account_repo.dart';
+export '../../pages/auth/createaccount/domain/usecase/create_account_usecase.dart';
+
+export '../../pages/auth/edit_profile/data/repoimpl/edit_profile_repo_impl.dart';
+export '../../pages/auth/edit_profile/domain/repositery/edit_profile_repositery.dart';
+export '../../pages/auth/edit_profile/domain/usecase/edit_profile_usecase.dart';
+
+export '../../pages/auth/forgotpassword/data/repoimpl/forgot_password_repo_impl.dart';
+export '../../pages/auth/forgotpassword/domain/repositery/forgot_password_repo.dart';
+export '../../pages/auth/forgotpassword/domain/usecase/forgot_password_usecase.dart';
+
+export '../../pages/auth/login/data/repoimpl/login_repo_impl.dart';
+export '../../pages/auth/login/domain/repositery/login_repo.dart';
+export '../../pages/auth/login/domain/usecase/login_usecase.dart';
+
+export '../../pages/auth/otpverification/data/repoimpl/verify_otp_rep_impl.dart';
+export '../../pages/auth/otpverification/domain/repositery/verify_otp_repositery.dart';
+export '../../pages/auth/otpverification/domain/usecase/verify_otp_usecase.dart';
+
+export '../../pages/auth/resetpassword/data/respoimpl/reset_password_impl.dart';
+export '../../pages/auth/resetpassword/domain/repositery/reset_password_repositery.dart';
+export '../../pages/auth/resetpassword/domain/usecase/reset_password_usecase.dart';
+
+// 📌 Parents - Application
+export '../../pages/parents/application/data/repoimpl/application_repositery_impl.dart';
+export '../../pages/parents/application/domain/repositery/application_repositery.dart';
+export '../../pages/parents/application/domain/usecase/application_usecase.dart';
+
+// 📌 Parents - Location
+export '../../pages/parents/location/data/repoImpl/location_repo_impl.dart';
+export '../../pages/parents/location/domain/repositery/location_repositery.dart';
+export '../../pages/parents/location/domain/usecase/location_usecase.dart';
+
+// 📌 Parents - Session Management
+export '../../pages/parents/session/add_detail/data/repo_impl/add_view_player_repo_impl.dart';
+export '../../pages/parents/session/add_detail/domain/repositery/add_view_player_repositery.dart';
+export '../../pages/parents/session/add_detail/domain/usecase/add_view_player_usecase.dart';
+
+export '../../pages/parents/session/calendar/data/repo_impl/session_calendar_date_repo_impl.dart';
+export '../../pages/parents/session/calendar/domain/repositery/session_calendar_dates_repositery.dart';
+export '../../pages/parents/session/calendar/domain/usecase/session_calendar_usecase.dart';
+
+export '../../pages/parents/session/coaching_detail/data/repo_impl/coaching_programing_detail_repo_impl.dart';
+export '../../pages/parents/session/coaching_detail/domain/repositery/coaching_detail_repositery.dart';
+export '../../pages/parents/session/coaching_detail/domain/usecase/coaching_program_detail_usecase.dart';
+
+export '../../pages/parents/session/coachprograms/data/repoImpl/coach_programs_repo_impl.dart';
+export '../../pages/parents/session/coachprograms/domain/repositery/coach_programs_repo.dart';
+export '../../pages/parents/session/coachprograms/domain/usecase/coach_programs_usecase.dart';
+
+export '../../pages/parents/session/order_summary/data/repo_impl/order_summary_repo_impl.dart';
+export '../../pages/parents/session/order_summary/domain/repositry/order_summary_repositery.dart';
+export '../../pages/parents/session/order_summary/domain/usecase/order_summary_usecase.dart';
+
+// 📌 Network
+export '../network/api_services.dart';
+export '../network/app_constant.dart';
+export 'package:get_it/get_it.dart';
